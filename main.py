e96e83331509314b4353f71347cad172ebe51923
Willis42 <willmharvey@gmail.com>
2016-09-29 01:02:11 -0500

    added logic for downloading thumbnail, and for just passing a url and letting it figure out the rest (still need to delete the files though)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import argparse
 import json
 import re
+import os
+from urllib import request
 
 #dependencies
 import youtube_dl
@@ -15,7 +17,7 @@ def download_video(url):
 
 def tracks_from_description(desc):
     tracklist = desc[desc.find('Tracklist'):]
-    tracks = [tuple(re.split('(?<=\d\d:\d\d)\s+', t)) for t in tracklist.split('\n') if re.fullmatch('\d\d:\d\d.+', t)]
+    tracks = [tuple(re.split('(?<=\d:\d\d)\s+', t)) for t in tracklist.split('\n') if re.fullmatch('\d+:\d\d.+', t)]
     return tracks
 
 def get_start_time_in_millis(track):
@@ -25,13 +27,25 @@ def get_start_time_in_millis(track):
 
 
 
-def split_from_filenames(video_filename, info_filename, outdir=''):
-    info = None
-    with open(info_filename) as info_file:
-        info = json.load(info_file)
+def split_from_filenames(video_filename, info_filename=None, outdir=None, info = None):
+    assert((info_filename or info) is not None)
+    
+    if info is None:
+        with open(info_filename) as info_file:
+            info = json.load(info_file)
+    
+    if outdir is None:
+        outdir = info['title'] + '/'
 
     tracks = tracks_from_description(info['description'])
     print(tracks)
+    
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+        
+    with request.urlopen(info['thumbnail']) as thumbnail_file:
+        with open(outdir + 'thumbnail_img.jpg', 'wb') as out:
+            out.write(thumbnail_file.read())
     
     audio = pydub.AudioSegment.from_file(video_filename)
     for i in range(len(tracks)):
@@ -41,7 +55,14 @@ def split_from_filenames(video_filename, info_filename, outdir=''):
             end = get_start_time_in_millis(tracks[i+1])
 
         piece = audio[start:end]
-        artist, title = tuple(re.split('\s*-\s*', tracks[i][1]))
+        title = tracks[i][1]
+        artist = ''
+        as_tuple = tuple(re.split('\s*-\s*', title))
+        if len(as_tuple) == 2:
+            artist = as_tuple[0]
+            title = as_tuple[1]
+        if len(as_tuple) > 2:
+            print('WARNING: split title has more than 2 parts:', as_tuple)
         tags = {'artist': artist, 'title': title, 'album': info['title'], 'track': i+1}
         filename = outdir + tracks[i][1] + '.mp3'
         
@@ -52,18 +73,27 @@ def split_from_filenames(video_filename, info_filename, outdir=''):
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--url', help='url of the YouTube video to be downloaded')
     arg_parser.add_argument('--vidfile', help='path of the input video file to be split into an album')
     arg_parser.add_argument('--infofile', help='path of the info file')
     arg_parser.add_argument('--outdir', help='path of the directory to output the album into')
     
     args=arg_parser.parse_args()
-    video_filename = args.vidfile
-    if video_filename is None:
-        video_filename = input('what\'s the path for the video file? ')
-    info_filename = args.infofile
-    if info_filename is None:
-        info_filename = input('what\'s the path for the info file? ')
-    outdir = args.outdir or ''
+    if args.url:
+        info = download_video(args.url)
+        video_filename = info['title'] + '-' + info['display_id'] + '.mkv'
+        info_filename = info['title'] + '-' + info['display_id'] + '.json'
+        with open(info_filename, 'w') as out:
+            json.dump(info, out)
+    else:
+        video_filename = args.vidfile
+        if video_filename is None:
+            video_filename = input('what\'s the path for the video file? ')
+        info_filename = args.infofile
+        if info_filename is None:
+            info_filename = input('what\'s the path for the info file? ')
+            
+    outdir = args.outdir
     
     print('OK! attempting to split...')
     split_from_filenames(video_filename, info_filename, outdir)
